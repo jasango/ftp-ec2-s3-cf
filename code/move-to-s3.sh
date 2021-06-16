94af7042fb593645b65ac67a9a45b559ec1e13b8
Javier Santana <jasango@gmail.com>
2021-06-16 00:00:15 -0400

    commit 06-16-21-00-00

--- code/move-to-s3.sh
+++ code/move-to-s3.sh
@@ -2,20 +2,23 @@
 now=$(date)
 text="ejecucion $now"
 echo $text >> ftp.log
-FILES="/home/ec2-user/ftp/datos/*"
-for f in $FILES
-do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  aws s3 cp "$f" s3://jasango-webcam
-  sudo rm -f "$f"
+for folder in /home/ec2-user/ftp/* ; do
+    echo "$folder"
+    SUBFOLDERS="$folder/*"
+    for subfolder in $SUBFOLDERS
+    do
+      echo "Processing $subfolder subfolder..."
+      sufix="${subfolder:19}"
+      aws s3 sync "$subfolder" s3://jasango-webcam/$sufix --storage-class STANDARD_IA
+      sudo rm -rf "$subfolder"
+    done
 done
 #check service running
 command="sudo ps -aux | grep -c 'sudo python -u ftp.py'"
-result=$(eval "$command") 
+result=$(eval "$command")
 if [ "$result" -eq 1 ]
 then
    echo 'service not running'
    sudo chown ec2-user password.txt
-   sudo '/home/ec2-user/ftp.sh' &  
-fi	
+   sudo '/home/ec2-user/ftp.sh' &
+fi
